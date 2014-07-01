945c0eb3ad9e679439ce3ee4ec4ecbd442d4aab1
Andreas Fitzek <andreas.fitzek@iaik.tugraz.at>
2014-07-01 10:12:35 +0200

    Configuration changes unified Config Constants interface

--- pdf-as-lib/src/main/java/at/gv/egiz/pdfas/lib/api/IConfigurationConstants.java
+++ pdf-as-lib/src/main/java/at/gv/egiz/pdfas/lib/api/IConfigurationConstants.java
@@ -23,6 +23,9 @@
  ******************************************************************************/
 package at.gv.egiz.pdfas.lib.api;
 
+/**
+ * Collection of useful configuration key
+ */
 public interface IConfigurationConstants {
 
 	public static final String TRUE = "true";
@@ -41,4 +44,38 @@ public interface IConfigurationConstants {
 	
 	public static final String PLACEHOLDER_SEARCH_ENABLED = "enable_placeholder_search";
 	public static final String DEFAULT_SIGNATURE_PROFILE = SIG_OBJECT + SEPERATOR + TYPE + SEPERATOR + DEFAULT;
+	
+	/**
+	 * Signature object Prefix
+	 */
+	public static final String SIG_OBJECT_PREFIX = "sig_obj.";
+	
+	/**
+	 * MOA SS Signing Key Identifier
+	 */
+	public static final String MOA_SS_KEY_IDENTIFIER = "moa.sign.KeyIdentifier";
+	
+	/**
+	 * MOA SS Signing Certificate
+	 */
+	public static final String MOA_SS_KEY_CERTIFICATE = "moa.sign.Certificate";
+	
+	
+	/**
+	 * MOA Trustprofile configuration Key
+	 */
+	public static final String MOA_TRUSTPROFILE_ID = "moa.verify.TrustProfileID";
+	
+	
+	/**
+	 * MOA Verify URL configuration Key
+	 */
+	public static final String MOA_VERIFY_URL = "moa.verify.url";
+	
+	
+	/**
+	 * MOA Sign URL configuration Key
+	 */
+	public static final String MOA_SIGN_URL = "moa.sign.url";
+	
 }
