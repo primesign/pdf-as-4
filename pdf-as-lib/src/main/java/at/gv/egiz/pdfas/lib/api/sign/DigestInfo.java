c4018ac43870eb27419b6f48c074b9ae692e8d84
Thomas Knall <thomas.knall@prime-sign.com>
2022-05-03 14:27:51 +0200

    Add JSR-305 annotations

--- pdf-as-lib/src/main/java/at/gv/egiz/pdfas/lib/api/sign/DigestInfo.java
+++ pdf-as-lib/src/main/java/at/gv/egiz/pdfas/lib/api/sign/DigestInfo.java
@@ -1,13 +1,17 @@
 package at.gv.egiz.pdfas.lib.api.sign;
 
+import javax.annotation.Nonnull;
+
 import iaik.asn1.structures.AlgorithmID;
 
 // TODO[PDFAS-114]: Add javadoc
 
 public interface DigestInfo {
 
+	@Nonnull
 	AlgorithmID getAlgorithm();
 
+	@Nonnull
 	byte[] getValue();
 
 }
