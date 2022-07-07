d9daea82fde55f59f55080bd4c3078e947cd9e79
Thomas Knall <thomas.knall@prime-sign.com>
2022-07-07 13:14:19 +0200

    Add javadoc

--- pdf-as-lib/src/main/java/at/gv/egiz/pdfas/lib/impl/signing/PDFASSignatureExtractor.java
+++ pdf-as-lib/src/main/java/at/gv/egiz/pdfas/lib/impl/signing/PDFASSignatureExtractor.java
@@ -3,11 +3,15 @@ package at.gv.egiz.pdfas.lib.impl.signing;
 import javax.annotation.Nullable;
 
 public interface PDFASSignatureExtractor extends PDFASSignatureInterface {
-	
+
 	byte[] getSignatureData();
 
-	// TODO[PDFAS-114]: Add javadoc
+	/**
+	 * Returns the byte range that should be used for signature digest calculation.
+	 * 
+	 * @return The byte range. (may be {@code null}).
+	 */
 	@Nullable
 	int[] getByteRange();
-	
+
 }
